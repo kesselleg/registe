d13889810bcdf976ff3894e2d242ff2bff7bf760
kesselleg <95750431+kesselleg@users.noreply.github.com>
2024-02-19 23:25:39 -0800

    nkjk

--- App.js
+++ App.js
@@ -7,22 +7,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
 
 
 export default function App() {
   return(
      
-<NavigationContainer>
-<Stack.Navigator>
-        <Stack.Screen name='login' component={Login}/>
 
-        <Stack.Screen name='register' component={Registers}/>
-        <Stack.Screen name='home' component={Home}/>
-      </Stack.Navigator>
-</NavigationContainer>
 
+
+<Home />
 
   )
 }
